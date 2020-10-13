4fde470b6bd44d29ecdd8d3d85d7d3f3e4c76b82
Kanishka Panamaldeniya <kpanamaldeniya@forerunner.com.au>
2020-10-13 14:30:03 +1030

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app-uploads-kanishka"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://voy20b2e64.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_Goa7We2sN",
+        APP_CLIENT_ID: "1v0g54q9b5ngm8rf82a3bvil61",
+        IDENTITY_POOL_ID: "us-east-2:3666ba41-6bac-4279-bcb6-97e9658719f6"
+    }
+};
